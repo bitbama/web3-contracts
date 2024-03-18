54bcfb8ef4ab167e0776093ca3b5583fd6b359dd
mike-obas <obademimichael@gmail.com>
2024-03-18 16:24:42 +0100

    Made fake key more glaring

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,7 +17,7 @@ const GOERLI_RPC_URL =
   "https://eth-goerli.g.alchemy.com/v2/YOUR-API-KEY"
 const PRIVATE_KEY =
   process.env.PRIVATE_KEY ||
-  "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+  "0x11ee3108a03081fanythinge260ecdc106554d09damything9d1209bcafd4694"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 const config: HardhatUserConfig = {
